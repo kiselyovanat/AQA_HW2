5ae5839352359545b5d6375ca25625d5cc72b441
Kiseleva Natalia <kiselyov-natalya@mail.ru>
2023-02-16 00:36:48 +0300

    Goodbye

--- src/main/java/ru/kiselevanat/Main.java
+++ src/main/java/ru/kiselevanat/Main.java
@@ -2,11 +2,6 @@ package ru.kiselevanat;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Goodbye world!");
-=======
-        System.out.println("Hello Mom!");
-        System.out.println("Hello Dad!");
->>>>>>> master
+        System.out.println("Hello world!");
     }
 }
