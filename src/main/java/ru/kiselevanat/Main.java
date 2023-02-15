77c169ad16d88b34729a5788860f79808f152cc6
Kiseleva Natalia <kiselyov-natalya@mail.ru>
2023-02-16 00:15:53 +0300

    new Hello msg

--- src/main/java/ru/kiselevanat/Main.java
+++ src/main/java/ru/kiselevanat/Main.java
@@ -2,6 +2,6 @@ package ru.kiselevanat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Mom!");
     }
 }
